a4ca7a43a746742835b0517898cd8100c6890dba
Gbsyi <Gbsyi@DESKTOP-NTRVV66>
2021-07-29 16:50:49 +0700

    delete

--- src/router.js
+++ src/router.js
@@ -29,7 +29,7 @@ router.post('/requests',(req,res)=>{
         }).catch((e) =>{
             res.json({
                 type:"Error",
-                message:e.message
+                error:e
             });
         });
 
@@ -49,13 +49,13 @@ router.get('/requests',(req,res) => {
         }).catch((e) => {
             res.json({
                 type: "Error",
-                message: e.message
+                message: e
             });
         });
     }catch(e){
         res.json({
             type:"Error",
-            message:e.message
+            error:e
         });
     }
 });
@@ -63,34 +63,82 @@ router.get('/requests',(req,res) => {
 //Изменение информации о доме
 router.put('/requests/:id',(req,res) =>{
     try{
-        let id = req.params;
+        let params = req.params;
+        if(!params.id) return res.status(404).json({
+            type:"Error",
+            message:"Missing id."
+        })
         let {name, description, type} = req.query;
         if(!name && !description && !type){
-            return res.json({
+            return res.status(400).json({
                 type:"Missing",
                 message:"There must be at least one argument."
             })
         }
-        let sql =`UPDATE houses SET ${name ? 'name=\'' + name + '\'' : ''} ${description ? `${!name ? '' : ','}description=\'` + description + '\'' : ''} ${type ? `${!name && !description ? '' : ','}type=\'`+type + '\'' : ''} WHERE houses.id = 1 `;
+        let sql =`UPDATE houses SET ${name ? 'name=\'' + name + '\'' : ''} ${description ? `${!name ? '' : ','}description=\'` + description + '\'' : ''} ${type ? `${!name && !description ? '' : ','}type=\'`+type + '\'' : ''} WHERE houses.id = ${params.id} `;
         pool.query(sql)
         .then((result) => {
-            res.json({
+            if(result[0].changedRows == 0){
+                return res.status(404).json({
+                    type:"Error",
+                    message:"Unknown id"
+                })
+            }
+            res.status(201).json({
                 type:"Success",
                 response_args:result[0]
             });
         }).catch((e) => {
-            res.json({
+            if(e.code == 'ER_BAD_FIELD_ERROR'){
+                return res.status(404).json({
+                    type:"Error",
+                    message:"Bad field"
+                })
+            }
+            res.status(500).json({
                 type:"Error",
-                message:e.message
+                error:e
             });
         })
     }catch(e){
         res.json({
             type:"Error",
-            message: e.message
+            error:e
         });
     }
 });
-router.delete('/requests');
+router.delete('/requests/:id', (req,res) => {
+    try{
+        let id = req.params.id;
+        if(!id) 
+            return res.status(404).json({
+                type:"Error",
+                message:"Missing id."
+            })
+        pool.query("DELETE FROM `houses` WHERE `id`=?",[id]).then((result) =>{
+            res.status(201).json({
+                type:"Success",
+                response_args:result[0]
+            })
+        }).catch((e) => {
+            if(e.code == 'ER_BAD_FIELD_ERROR'){
+                return res.status(404).json({
+                    type:"Error",
+                    message:"Bad field"
+                })
+            }
+            res.status(500).json({
+                type:"Error",
+                error:e
+            })
+        });
+    }catch(e){
+        console.log(e.message)
+        res.status(500).json({
+            type:"Error",
+            error:e
+        })
+    }
+});
 
 export default router;
